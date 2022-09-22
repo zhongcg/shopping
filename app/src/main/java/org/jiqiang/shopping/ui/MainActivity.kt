6b29d725ae1dc02ec5d2ec154867c6802f4e9f7f
zhongcg <zhongcg@qq.com>
2022-09-22 18:10:09 +0800

    init

--- app/src/main/java/org/jiqiang/shopping/ui/MainActivity.kt
+++ app/src/main/java/org/jiqiang/shopping/ui/MainActivity.kt
@@ -2,11 +2,21 @@ package org.jiqiang.shopping.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.jiqiang.shopping.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.title
+        binding.topBar.title = "Title"
+
+
 
     }
 }
