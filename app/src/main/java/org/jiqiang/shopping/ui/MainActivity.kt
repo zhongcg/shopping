8d34bb6e59f0da126a94c5deb5312a01ccd0781a
zhongcg <zhongcg@qq.com>
2022-09-22 17:54:05 +0800

    init

---
+++ app/src/main/java/org/jiqiang/shopping/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.jiqiang.shopping.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
